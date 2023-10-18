8b8677189c3fdf8a5976fefe7fd7206e57f58048
Ian Young <ian@iangreenleaf.com>
2023-10-17 21:35:52 -0500

    Slim down the requested fields from Pocket
    I wasn't using all the other stuff, it just came with the example
    request.

--- index.ts
+++ index.ts
@@ -49,54 +49,9 @@ const listArticles = gql`
     }
   }
   fragment ItemDetails on Item {
-    isArticle
     title
-    itemId
-    resolvedId
-    resolvedUrl
-    domain
-    domainMetadata {
-      name
-    }
-    excerpt
-    hasImage
-    hasVideo
-    images {
-      caption
-      credit
-      height
-      imageId
-      src
-      width
-    }
-    videos {
-      vid
-      videoId
-      type
-      src
-    }
-    topImageUrl
-    timeToRead
     givenUrl
-    collection {
-      imageUrl
-      intro
-      title
-      excerpt
-    }
-    authors {
-      id
-      name
-      url
-    }
     datePublished
-    syndicatedArticle {
-      slug
-      publisher {
-        name
-        url
-      }
-    }
   }
 `;
 
